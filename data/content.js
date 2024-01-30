4be6a3173238ec236e8a75e4a7d9b1dac67a710b
aneezsidhik <sidhik.aneez@gmail.com>
2024-01-30 00:12:32 +0000

    fixing js errors

--- data/content.js
+++ data/content.js
@@ -23,7 +23,7 @@ App.content = [
     },
     {
         "image": "isha_weekend_630.jpg",
-        "fromDate": "2023-11-05 18:30"
+        "fromDate": "2023-11-05 18:30",
         "toDate": "2024-01-28 18:30"
     }
 /*    {
