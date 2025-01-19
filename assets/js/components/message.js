2fdefaaebb6c8c1dd79904590ec245d3aa598fed
aneezsidhik <sidhik.aneez@gmail.com>
2025-01-19 00:24:21 +0000

    to temporarily display screen size

--- assets/js/components/message.js
+++ assets/js/components/message.js
@@ -5,11 +5,12 @@ class MessageComponent {
     this.currentMessageIndex = 0;
     this.twoTabs = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
     this.jumaTimes = `JUM'A KHUTBA TIMES: `.concat(utils.convertToTwelveHourTime(App.todaysData.juma[0]), ' & ', utils.convertToTwelveHourTime(App.todaysData.juma[1]));
-    this.messages = 'PLEASE OBSERVE SILENCE IN THE MASJID'.concat(this.twoTabs,this.jumaTimes);
+    this.screenSize = window.screen.availWidth +' x ' + window.screen.availHeight;
+    this.messages = ['PLEASE OBSERVE SILENCE IN THE MASJID', this.jumaTimes, this.screenSize];
 
     if (App.todaysData.message) {
-      this.messages = this.messages.concat(this.twoTabs,App.todaysData.message);
+      this.messages.push(App.todaysData.message);
     }
-    this.containerElement.innerHTML = this.messages;
+    this.containerElement.innerHTML = this.messages.join(this.twoTabs);
   }
 }
