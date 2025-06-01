a46145bfeb623c1a1e559ed40bb5f8a38a678d0b
aneezsidhik <sidhik.aneez@gmail.com>
2025-06-01 20:43:12 +0100

    removed event day poster

--- data/content.js
+++ data/content.js
@@ -2,11 +2,7 @@ App.content = [
     {
         "image": "eid.jpg",
         "toDate": "2025-06-06 09:59:00"
-    },
-    {
-        "image": "event_days.jpg",
-        "toDate": "2025-05-31 00:00:00"
-    },
+    }
     {
         "image": "juma_summer.jpg"
     },
